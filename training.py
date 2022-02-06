1dbfcf5cfdbca01352c7a137b1cd178312c5aee4
Martina Grganic <totalnobzvz@yahoo.com>
2022-02-06 19:00:32 +0000

    Added lemmatize to get word by word printed

--- training.py
+++ training.py
@@ -22,8 +22,13 @@ ignore_letters = ["!", "?", ".", ","]
 for intent in intents["intents"]:
     for pattern in intent["patterns"]:
         word_list = nltk.word_tokenize(pattern)
-        words.append(word_list)
+        words.extend(word_list)
         documents.append((word_list, intent['tag']))
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
-print(documents)
+
+
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+print(words)
